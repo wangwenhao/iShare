5208d530dccc84f9dad8432b2c6744117433be0a
wangwenhao <wangwenhao@live.cn>
2013-02-19 20:55:14 +0800

    Initial Commit

---
+++ iShare/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  iShare
+//
+//  Created by Bryant on 2/19/13.
+//  Copyright (c) 2013 NCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
